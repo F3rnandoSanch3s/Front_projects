d6bb8836062212d7529c84f437e5983cd017912d
Fernando Sanches <Sanchesfernando.work@gmail.com>
2023-12-07 12:52:39 -0300

    barra de pesquisa escondida

---
+++ pesquisa_escondida/script.js
@@ -0,0 +1,25 @@
+const pesquisa = document.querySelector('.pesquisa')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    pesquisa.classList.toggle('ativa')
+    input.focus()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
