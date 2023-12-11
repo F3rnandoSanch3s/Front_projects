dc0ecd5ded9367423f6459d28f8d6892f37a6bac
Fernando Sanches <Sanchesfernando.work@gmail.com>
2023-12-11 10:53:41 -0300

    slider vertical

---
+++ slider_vertical_duplo/script.js
@@ -0,0 +1,31 @@
+const sliderContainer = document.querySelector('.slider')
+const slideRight = document.querySelector('.slide_direita')
+const slideLeft = document.querySelector('.slide_esquerda')
+const upButton = document.querySelector('.botao_subir')
+const downButton = document.querySelector('.botao_descer')
+const slidesLength = slideRight.querySelectorAll('div').length
+
+let activeSlideIndex = 0
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
+
+upButton.addEventListener('click', () => changeSlide('up'))
+downButton.addEventListener('click', () => changeSlide('down'))
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderContainer.clientHeight
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
