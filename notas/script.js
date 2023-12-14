a777d0c0cd0e3e45d3aee6c4d3b8ce6201f1bf94
Fernando Sanches <Sanchesfernando.work@gmail.com>
2023-12-14 00:12:43 -0300

    att com comentarios

--- notas/script.js
+++ notas/script.js
@@ -1,18 +1,24 @@
-const addBtn = document.getElementById('add')
+const adicaoBtn = document.getElementById('add')
 
-const notes = JSON.parse(localStorage.getItem('notes'))
+// Recupera notas do armazenamento local e converte para array
+const notas = JSON.parse(localStorage.getItem('notes'))
 
-if(notes) {
-    notes.forEach(note => addNewNote(note))
+// Se existirem notas, adiciona cada nota usando a função addNovaNota
+if (notas) {
+    notas.forEach(nota => addNovaNota(nota))
 }
 
-addBtn.addEventListener('click', () => addNewNote())
+// Adiciona um ouvinte de evento de clique ao botão de adição
+adicaoBtn.addEventListener('click', () => addNovaNota())
 
-function addNewNote(text = '') {
-    const note = document.createElement('div')
-    note.classList.add('note')
+// Função para adicionar uma nova nota
+function addNovaNota(text = '') {
+    // Cria um novo elemento div para representar a nota
+    const nota = document.createElement('div')
+    nota.classList.add('note')
 
-    note.innerHTML = `
+    // Adiciona HTML à nota, incluindo ferramentas (editar e excluir) e área de texto
+    nota.innerHTML = `
     <div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
@@ -22,44 +28,60 @@ function addNewNote(text = '') {
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `
 
-    const editBtn = note.querySelector('.edit')
-    const deleteBtn = note.querySelector('.delete')
-    const main = note.querySelector('.main')
-    const textArea = note.querySelector('textarea')
+    // Obtém referências aos elementos dentro da nota
+    const editaBtn = nota.querySelector('.edit')
+    const deleteBtn = nota.querySelector('.delete')
+    const main = nota.querySelector('.main')
+    const AreaTexto = nota.querySelector('textarea')
 
-    textArea.value = text
+    // Define o valor da área de texto como o texto fornecido (ou vazio)
+    AreaTexto.value = text
+
+    // Converte o texto formatado para HTML usando a biblioteca "marked"
     main.innerHTML = marked(text)
 
+    // Adiciona um ouvinte de evento de clique para o botão de exclusão
     deleteBtn.addEventListener('click', () => {
-        note.remove()
+        // Remove a nota do DOM
+        nota.remove()
 
+        // Atualiza o armazenamento local após a exclusão
         updateLS()
     })
 
-    editBtn.addEventListener('click', () => {
+    // Adiciona um ouvinte de evento de clique para o botão de edição
+    editaBtn.addEventListener('click', () => {
+        // Alterna a visibilidade entre a visualização principal e a área de texto
         main.classList.toggle('hidden')
-        textArea.classList.toggle('hidden')
+        AreaTexto.classList.toggle('hidden')
     })
 
-    textArea.addEventListener('input', (e) => {
-        const { value } = e.target
+    // Adiciona um ouvinte de evento de entrada para a área de texto
+    AreaTexto.addEventListener('input', (e) => {
+        // Atualiza a visualização principal com o texto formatado
+        main.innerHTML = marked(e.target.value)
 
-        main.innerHTML = marked(value)
-
+        // Atualiza o armazenamento local com as alterações
         updateLS()
     })
 
-    document.body.appendChild(note)
+    // Adiciona a nota ao corpo do documento
+    document.body.appendChild(nota)
 }
 
+// Atualiza o armazenamento local com as informações mais recentes das notas
 function updateLS() {
-    const notesText = document.querySelectorAll('textarea')
+    // Obtém todas as áreas de texto presentes no documento
+    const TextoNotas = document.querySelectorAll('textarea')
 
-    const notes = []
+    // Cria um array para armazenar os valores das áreas de texto
+    const notas = []
 
-    notesText.forEach(note => notes.push(note.value))
+    // Para cada área de texto, adiciona seu valor ao array
+    TextoNotas.forEach(nota => notas.push(nota.value))
 
-    localStorage.setItem('notes', JSON.stringify(notes))
+    // Atualiza o armazenamento local com o array de valores em formato JSON
+    localStorage.setItem('notes', JSON.stringify(notas))
 }
 
 
